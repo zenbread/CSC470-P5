7ea2e97e1d156fbc5299056a4e0025ff016a6aa5
nick <nmcamp87@gmail.com>
2021-10-13 19:50:38 -0400

    Added iproject interface

---
+++ IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace P5
+{
+    interface IProjectRepository
+    {
+        string Add(Project project, out int Id);
+        string Remove(int projectId);
+        string Modify(int projectId, Project project);
+        List<Project> GetAll();
+        bool IsDuplicateName(string projectName);
+
+    }
+}
