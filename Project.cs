45209b849af5d2a1f69f565285540bfe3038d13f
nick <nmcamp87@gmail.com>
2021-10-13 19:40:09 -0400

    Added Project class to the forms

---
+++ Project.cs
@@ -0,0 +1,15 @@
+﻿namespace P5
+{
+    class Project
+    {
+
+        public Project(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+    }
+}
