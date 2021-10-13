86e8645f927648f19c6ddc93dc537ad5e0e79c08
nick <nmcamp87@gmail.com>
2021-10-13 19:42:01 -0400

    Added preference class

---
+++ Preference.cs
@@ -0,0 +1,16 @@
+﻿namespace P5
+{
+    class Preference
+    {
+        public Preference(string username, string name, string value)
+        {
+            Username = username;
+            Name = name;
+            Value = value;
+        }
+
+        public string Username { get; }
+        public string Name { get; }
+        public string Value { get; }
+    }
+}
