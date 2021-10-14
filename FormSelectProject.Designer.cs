2aa94a2781f3bbcec1d354d8df46d6779e97cf76
nick <nmcamp87@gmail.com>
2021-10-14 19:41:02 -0400

    Made a select project form

---
+++ FormSelectProject.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace P5
+{
+    partial class FormSelectProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxProjects = new System.Windows.Forms.ListBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxProjects
+            // 
+            this.listBoxProjects.FormattingEnabled = true;
+            this.listBoxProjects.Location = new System.Drawing.Point(24, 36);
+            this.listBoxProjects.Name = "listBoxProjects";
+            this.listBoxProjects.Size = new System.Drawing.Size(574, 238);
+            this.listBoxProjects.TabIndex = 0;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(233, 296);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(134, 35);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(445, 296);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(134, 35);
+            this.buttonSelect.TabIndex = 2;
+            this.buttonSelect.Text = "Select Project";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // FormSelectProject
+            // 
+            this.AcceptButton = this.buttonSelect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(626, 357);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.listBoxProjects);
+            this.Name = "FormSelectProject";
+            this.Text = "Select Project";
+            this.Load += new System.EventHandler(this.SelectProjectForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProjects;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonSelect;
+    }
+}
---
+++ FormSelectProject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace P5
+{
+    public partial class FormSelectProject : Form
+    {
+        public Project ReturnProject;
+        public FormSelectProject()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectProjectForm_Load(object sender, EventArgs e)
+        {
+            CenterToParent();
+            FakeProjectRepository repo = new FakeProjectRepository();
+
+            List<Project> projects = repo.GetAll();
+
+            listBoxProjects.DataSource = projects;
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            ReturnProject = (Project)listBoxProjects.SelectedItem;
+            DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
