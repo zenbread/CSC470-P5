d012c7fa3f69864f85cc63ce6880a3dbd86e8769
nick <nmcamp87@gmail.com>
2021-10-13 21:47:13 -0400

    Implemented the Fake preference repository

---
+++ FakePreferenceRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace P5
+{
+    class FakePreferenceRepository : IPreferenceRepository
+    {
+        public const string PREFERENCE_PROJECT_ID = "Project_Id";
+        public const string PREFERENCE_PROJECT_NAME = "Project_Name";
+        private const string NO_ERROR = "";
+        private const string EMPTY_ERROR = "EMPTY";
+        private static Dictionary<string, Dictionary<string, string>> preferences = new Dictionary<string, Dictionary<string, string>>();
+        string IPreferenceRepository.GetPreference(string UserName, string PreferenceName)
+        {
+            Dictionary<string, string> NameValuePair;
+            string value = "";
+            if (preferences.TryGetValue(UserName, out NameValuePair))
+            {
+                NameValuePair.TryGetValue(PreferenceName, out value);
+            }
+            return value;
+        }
+
+        string IPreferenceRepository.SetPreference(string UserName, string PreferenceName, string Value)
+        {
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(PreferenceName) || string.IsNullOrEmpty(Value))
+                return EMPTY_ERROR;
+            Dictionary<string, string> NameValuePair;
+            if (!preferences.TryGetValue(UserName, out NameValuePair))
+            {
+                NameValuePair.Add(PreferenceName, Value);
+                preferences.Add(UserName, NameValuePair);
+                return NO_ERROR;
+            }
+            NameValuePair = new Dictionary<string, string>
+            {
+                { PreferenceName, Value }
+            };
+            preferences.Add(UserName, NameValuePair);
+            return NO_ERROR;
+        }
+    }
+}
