bc8fcfa32aeee3ac8892a024df70e06c4027367c
nick <nmcamp87@gmail.com>
2021-10-14 22:15:26 -0400

    Added new modify label and form

---
+++ FormModifyProject.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace P5
+{
+    partial class FormModifyProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Name:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(244, 85);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(126, 32);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(422, 85);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(126, 32);
+            this.btnModify.TabIndex = 3;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(97, 43);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(451, 20);
+            this.textBox.TabIndex = 4;
+            // 
+            // FormModifyProject
+            // 
+            this.AcceptButton = this.btnModify;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(613, 129);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label1);
+            this.Name = "FormModifyProject";
+            this.Text = "Modify Project";
+            this.Load += new System.EventHandler(this.FormRemoveProject_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
---
+++ FormModifyProject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P5
+{
+    public partial class FormModifyProject : Form
+    {
+        FormMain parent;
+        public FormModifyProject()
+        {
+            InitializeComponent();
+        }
+
+        private void FormRemoveProject_Load(object sender, EventArgs e)
+        {
+            CenterToParent();
+            parent = (FormMain)this.Owner;
+            textBox.Text = parent.selected.Name;
+            ActiveControl = textBox;
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            Project tmp = new Project(textBox.Text.Trim());
+            string msg = parent.projectRepo.Modify(parent.selected.Id, tmp);
+           
+            if (!string.IsNullOrEmpty(msg))
+            {
+                MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+            MessageBox.Show("Project modified successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+    }
+}
