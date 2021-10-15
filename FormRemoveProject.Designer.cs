5cebeea39ce1242d1cee8670e7960cf87f6d6822
nick <nmcamp87@gmail.com>
2021-10-14 21:37:07 -0400

    Updated all form to handle remove

---
+++ FormRemoveProject.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace P5
+{
+    partial class FormRemoveProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.labelProjectToRemove = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Name:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(244, 85);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(126, 32);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(422, 85);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(126, 32);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // labelProjectToRemove
+            // 
+            this.labelProjectToRemove.AutoSize = true;
+            this.labelProjectToRemove.Location = new System.Drawing.Point(97, 43);
+            this.labelProjectToRemove.Name = "labelProjectToRemove";
+            this.labelProjectToRemove.Size = new System.Drawing.Size(35, 13);
+            this.labelProjectToRemove.TabIndex = 4;
+            this.labelProjectToRemove.Text = "label2";
+            // 
+            // FormRemoveProject
+            // 
+            this.AcceptButton = this.btnRemove;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(613, 129);
+            this.Controls.Add(this.labelProjectToRemove);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label1);
+            this.Name = "FormRemoveProject";
+            this.Text = "Remove Project";
+            this.Load += new System.EventHandler(this.FormCreateProject_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label labelProjectToRemove;
+    }
+}
---
+++ FormRemoveProject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P5
+{
+    public partial class FormRemoveProject : Form
+    {
+        FormMain parent;
+        public FormRemoveProject()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Project tmp = new Project(textBox.Text.Trim());
+            string msg = parent.projectRepo.Add(tmp, out int id);
+            if (id < 0)
+            {
+                MessageBox.Show(msg, "Attention");
+                return;
+            }
+            MessageBox.Show($"{tmp.Name} added to projects.");
+            this.Close();
+        }
+
+        private void FormCreateProject_Load(object sender, EventArgs e)
+        {
+            CenterToParent();
+            parent = (FormMain)this.Owner;
+        }
+    }
+}
