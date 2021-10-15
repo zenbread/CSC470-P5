7df5aef8d9161caa18f7f19c939d02f9941d2e15
nick <nmcamp87@gmail.com>
2021-10-14 21:38:01 -0400

    Removed erroneous methods from remove form

--- FormRemoveProject.cs
+++ FormRemoveProject.cs
@@ -10,23 +10,6 @@ namespace P5
         {
             InitializeComponent();
         }
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            Project tmp = new Project(textBox.Text.Trim());
-            string msg = parent.projectRepo.Add(tmp, out int id);
-            if (id < 0)
-            {
-                MessageBox.Show(msg, "Attention");
-                return;
-            }
-            MessageBox.Show($"{tmp.Name} added to projects.");
-            this.Close();
-        }
 
-        private void FormCreateProject_Load(object sender, EventArgs e)
-        {
-            CenterToParent();
-            parent = (FormMain)this.Owner;
-        }
     }
 }
